a6a8cda0fb5ab62ffb91c96b1a73d9546e1094e6
Christian Y <christiangandha@gmail.com>
2021-03-20 08:43:13 +0700

    -background tween alpha on spirit

---
+++ New Unity Project/Assets/Script/Tweening/TweenPingPongAlpha.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TweenPingPongAlpha : BaseTweenPingPongLoop
+{
+    [Space]
+    [SerializeField] private float alphaTo = 1f;
+    public override void StartTweening()
+    {
+        if (easingType == LeanTweenType.animationCurve)
+            _ltDescUniqID = LeanTween.alpha(gameObject, alphaTo, duration).setEase(animationCurve).setLoopPingPong(loopPingPong).uniqueId;
+        else
+            _ltDescUniqID = LeanTween.alpha(gameObject, alphaTo, duration).setEase(easingType).setLoopPingPong(loopPingPong).uniqueId;
+    }
+}
