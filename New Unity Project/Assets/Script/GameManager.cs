d6886fa0a2f0bfe44bd6dd5d513f8275f50e3626
DESKTOP-F66K4FM\PCrumah <rickyarebinds99@gmail.com>
2021-03-22 03:40:06 +0700

    Game manager -option "mau pakai particle" effect

--- New Unity Project/Assets/Script/GameManager.cs
+++ New Unity Project/Assets/Script/GameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public bool maupakaiparticle;
     public static GameManager sgtgamemanager;
     private void Awake()
     {
